9be2860301c61252d2ce57ac28f3370821213b86
carfreeman <119606311+carfreeman@users.noreply.github.com>
2024-02-08 10:44:28 -0500

    CLI
    actualizando CLI para usar variables de entorno

--- cli.php
+++ cli.php
@@ -1,39 +1,19 @@
 <?php
 
 include __DIR__ . '/src/Framework/Database.php';
+include __DIR__  . '/vendor/autoload.php';
 
 use Framework\Database;
+use Dotenv\Dotenv;
 
-$db = new Database('mysql', [
-    'host' => 'localhost',
-    'port' => 3306,
-    'dbname' => 'phpiggy'
-], 'root', '');
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-/* try {
-    $db->connection->beginTransaction();
-
-    $db->connection->query("INSERT INTO products VALUES(99, 'Gloves')");
-
-    $search = "Hats";
-    $query = "SELECT * FROM products WHERE name = :name";
-    
-    $stmt = $db->connection->prepare($query);
-    
-    $stmt->bindValue('name', 'Gloves', PDO::PARAM_STR);
-    
-    $stmt->execute();
-    
-    var_dump($stmt->fetchAll(PDO::FETCH_OBJ));
-
-    $db->connection->commit();
-} catch (Exception $error) {
-    if ($db->connection->inTransaction()) {
-        $db->connection->rollBack();
-    }
-
-    echo "Transaction failed!";
-} */
+$db = new Database($_ENV['DB_DRIVER'], [
+    'host' => $_ENV['DB_HOST'],
+    'port' => $_ENV['DB_PORT'],
+    'dbname' => $_ENV['DB_NAME']
+], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
 $sqlFile = file_get_contents('./database.sql');
 
